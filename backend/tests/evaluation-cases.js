11f78c6eb589810854d771ea816da5eb5dace17e
Khushi Shaikh <khushishaikh4860@gmail.com>
2026-08-24 13:40:24 +0530

    Create evaluation-cases.js

---
+++ backend/tests/evaluation-cases.js
@@ -0,0 +1,174 @@
+/*
+=========================================================
+FASHION AI DISCOVERY
+DAY 8
+BENCHMARK QUERIES
+=========================================================
+*/
+
+export const evaluationCases = [
+
+  {
+    query:
+      "black oversized shirt",
+
+    expected: {
+      category:
+        "Shirts",
+
+      color:
+        "Black",
+
+      style:
+        "Oversized",
+    },
+  },
+
+  {
+    query:
+      "comfortable summer shirt",
+
+    expected: {
+      category:
+        "Shirts",
+
+      style:
+        "Relaxed",
+
+      occasion:
+        "Summer",
+    },
+  },
+
+  {
+    query:
+      "minimal white sneakers",
+
+    expected: {
+      category:
+        "Sneakers",
+
+      color:
+        "White",
+
+      style:
+        "Minimal",
+    },
+  },
+
+  {
+    query:
+      "formal black blazer",
+
+    expected: {
+      category:
+        "Blazers",
+
+      color:
+        "Black",
+
+      style:
+        "Formal",
+    },
+  },
+
+  {
+    query:
+      "college casual jeans",
+
+    expected: {
+      category:
+        "Jeans",
+
+      occasion:
+        "College",
+
+      style:
+        "Casual",
+    },
+  },
+
+  {
+    query:
+      "grey oversized hoodie",
+
+    expected: {
+      category:
+        "Hoodies",
+
+      color:
+        "Grey",
+
+      style:
+        "Oversized",
+    },
+  },
+
+  {
+    query:
+      "elegant black evening dress",
+
+    expected: {
+      category:
+        "Dresses",
+
+      color:
+        "Black",
+
+      style:
+        "Elegant",
+
+      occasion:
+        "Evening",
+    },
+  },
+
+  {
+    query:
+      "white lightweight sneakers for travel",
+
+    expected: {
+      category:
+        "Sneakers",
+
+      color:
+        "White",
+
+      occasion:
+        "Travel",
+    },
+  },
+
+  {
+    query:
+      "minimal black trousers for office",
+
+    expected: {
+      category:
+        "Trousers",
+
+      color:
+        "Black",
+
+      occasion:
+        "Office",
+    },
+  },
+
+  {
+    query:
+      "cream summer dress",
+
+    expected: {
+      category:
+        "Dresses",
+
+      color:
+        "Cream",
+
+      occasion:
+        "Summer",
+    },
+  },
+
+];
