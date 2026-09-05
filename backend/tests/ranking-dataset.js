e0cd7b25538189c77753b07d700423eaad35b611
Khushi Shaikh <khushishaikh4860@gmail.com>
2026-09-05 15:35:20 +0530

    Add ranking dataset for product queries

---
+++ backend/tests/ranking-dataset.js
@@ -0,0 +1,308 @@
+const dataset = [
+    {
+        query:
+            "black formal shirt for office under 3000",
+        products: [
+            {
+                product: {
+                    id: 1,
+                    name:
+                        "Black Formal Shirt",
+                    brand:
+                        "Urban Mode",
+                    category:
+                        "shirt",
+                    gender:
+                        "men",
+                    color:
+                        "black",
+                    material:
+                        "cotton",
+                    style:
+                        "formal",
+                    occasion:
+                        "office",
+                    price:
+                        1999,
+                    description:
+                        "Classic black formal cotton shirt for office wear",
+                    tags: [
+                        "formal",
+                        "office",
+                        "black"
+                    ],
+                    semanticScore:
+                        0.92
+                },
+                relevance: 3
+            },
+            {
+                product: {
+                    id: 2,
+                    name:
+                        "Blue Casual Shirt",
+                    brand:
+                        "Daily Wear",
+                    category:
+                        "shirt",
+                    gender:
+                        "men",
+                    color:
+                        "blue",
+                    material:
+                        "cotton",
+                    style:
+                        "casual",
+                    occasion:
+                        "college",
+                    price:
+                        1499,
+                    description:
+                        "Comfortable blue casual shirt",
+                    tags: [
+                        "casual",
+                        "college"
+                    ],
+                    semanticScore:
+                        0.55
+                },
+                relevance: 1
+            },
+            {
+                product: {
+                    id: 3,
+                    name:
+                        "Black Party Dress",
+                    brand:
+                        "Style House",
+                    category:
+                        "dress",
+                    gender:
+                        "women",
+                    color:
+                        "black",
+                    material:
+                        "polyester",
+                    style:
+                        "party",
+                    occasion:
+                        "party",
+                    price:
+                        2499,
+                    description:
+                        "Elegant black party dress",
+                    tags: [
+                        "party"
+                    ],
+                    semanticScore:
+                        0.48
+                },
+                relevance: 0
+            }
+        ]
+    },
+    {
+        query:
+            "blue casual sneakers for college",
+        products: [
+            {
+                product: {
+                    id: 4,
+                    name:
+                        "Blue Casual Sneakers",
+                    brand:
+                        "Street Step",
+                    category:
+                        "sneakers",
+                    gender:
+                        "unisex",
+                    color:
+                        "blue",
+                    material:
+                        "mesh",
+                    style:
+                        "casual",
+                    occasion:
+                        "college",
+                    price:
+                        2499,
+                    description:
+                        "Comfortable blue sneakers for everyday college use",
+                    tags: [
+                        "casual",
+                        "college"
+                    ],
+                    semanticScore:
+                        0.91
+                },
+                relevance: 3
+            },
+            {
+                product: {
+                    id: 5,
+                    name:
+                        "White Formal Sneakers",
+                    brand:
+                        "Modern Walk",
+                    category:
+                        "sneakers",
+                    gender:
+                        "men",
+                    color:
+                        "white",
+                    material:
+                        "leather",
+                    style:
+                        "formal",
+                    occasion:
+                        "office",
+                    price:
+                        2999,
+                    description:
+                        "Clean white sneakers for office outfits",
+                    tags: [
+                        "formal",
+                        "office"
+                    ],
+                    semanticScore:
+                        0.51
+                },
+                relevance: 1
+            },
+            {
+                product: {
+                    id: 6,
+                    name:
+                        "Red Party Dress",
+                    brand:
+                        "Style House",
+                    category:
+                        "dress",
+                    gender:
+                        "women",
+                    color:
+                        "red",
+                    material:
+                        "polyester",
+                    style:
+                        "party",
+                    occasion:
+                        "party",
+                    price:
+                        2199,
+                    description:
+                        "Red party dress",
+                    tags: [
+                        "party"
+                    ],
+                    semanticScore:
+                        0.30
+                },
+                relevance: 0
+            }
+        ]
+    },
+    {
+        query:
+            "women cotton casual shirt under 2000",
+        products: [
+            {
+                product: {
+                    id: 7,
+                    name:
+                        "Women's Cotton Casual Shirt",
+                    brand:
+                        "Daily Mode",
+                    category:
+                        "shirt",
+                    gender:
+                        "women",
+                    color:
+                        "white",
+                    material:
+                        "cotton",
+                    style:
+                        "casual",
+                    occasion:
+                        "casual",
+                    price:
+                        1799,
+                    description:
+                        "Comfortable women's cotton casual shirt",
+                    tags: [
+                        "cotton",
+                        "casual"
+                    ],
+                    semanticScore:
+                        0.94
+                },
+                relevance: 3
+            },
+            {
+                product: {
+                    id: 8,
+                    name:
+                        "Women's Formal Blazer",
+                    brand:
+                        "Office Edit",
+                    category:
+                        "blazer",
+                    gender:
+                        "women",
+                    color:
+                        "black",
+                    material:
+                        "polyester",
+                    style:
+                        "formal",
+                    occasion:
+                        "office",
+                    price:
+                        1899,
+                    description:
+                        "Women's formal blazer",
+                    tags: [
+                        "formal",
+                        "office"
+                    ],
+                    semanticScore:
+                        0.42
+                },
+                relevance: 1
+            },
+            {
+                product: {
+                    id: 9,
+                    name:
+                        "Men's Denim Jeans",
+                    brand:
+                        "Urban Denim",
+                    category:
+                        "jeans",
+                    gender:
+                        "men",
+                    color:
+                        "blue",
+                    material:
+                        "denim",
+                    style:
+                        "casual",
+                    occasion:
+                        "casual",
+                    price:
+                        1599,
+                    description:
+                        "Classic men's denim jeans",
+                    tags: [
+                        "denim"
+                    ],
+                    semanticScore:
+                        0.25
+                },
+                relevance: 0
+            }
+        ]
+    }
+];
+
+export default dataset;
